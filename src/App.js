6edc414c7d05026aa87e8f2e5aafa66860fa297e
jinxcastro <work.rafaelcastro@gmail.com>
2024-03-11 15:02:50 +0800

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,66 +1,3 @@
-// import * as React from "react";
-// import Navbar from "react-bootstrap/Navbar";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import "bootstrap/dist/css/bootstrap.css";
-// import { FaArrowRight } from 'react-icons/fa';
-// import './App.css'
-
-// import { BrowserRouter as Router , Routes, Route, Link, useLocation } from "react-router-dom";
-
-// import EditAccount from "./components/product/edit.component";
-// import AccountList from "./components/product/list.component";
-// import CreateAccount from "./components/product/create.component";
-// import DragLayout from "./Login/dragLayout";
-
-// function App() {
-//   return (
-//     <Router>
-//       <MainPage />
-//     </Router>
-//   );
-// }
-
-// function MainPage() {
-//   const location = useLocation();
-//   // const showNavbar = location.pathname !== "/";
-
-//   return (
-//     <>
-//       {/* {showNavbar && (  */}
-//         <Navbar className="navbar-header">
-//           <Container>
-//             <Link to={"/product/list/:id"} className="navbar-brand text-white">
-//               <strong>Drag Layout</strong>
-//             </Link>
-//             {/* <div className="ml-auto">
-//               <Link to={"/"} className="btn navbar-header">
-//                 Welcome Banker <FaArrowRight />
-//               </Link>
-//             </div> */}
-//           </Container>
-//         </Navbar>
-//       {/* )} */}
-
-//       <Container className="mt-5">
-//         <Row>
-//           <Col md={12}>
-//             <Routes>
-//               <Route path="/product/create" element={<CreateAccount />} />
-//               <Route path="/product/edit/:id" element={<EditAccount />} />
-//               <Route path="/product/list/:id" element={<AccountList />} />
-//               <Route exact path='/' element={<DragLayout />} />
-//             </Routes>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import * as React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -69,10 +6,6 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 
 import { BrowserRouter as Router , Routes, Route, Link } from "react-router-dom";
-
-import EditProduct from "./components/product/edit.component";
-import ProductList from "./components/product/list.component";
-import CreateProduct from "./components/product/create.component";
 import DragLayout from "./Login/dragLayout";
 
 function App() {
@@ -82,9 +15,6 @@ function App() {
         <Link to={"/"} className="navbar-brand text-white">
           Basic Crud App
         </Link>
-        <Link to={"/Login/dragLayout"} className="navbar-brand text-white">
-          Oten
-        </Link>
       </Container>
     </Navbar>
 
@@ -92,10 +22,7 @@ function App() {
       <Row>
         <Col md={12}>
           <Routes>
-            <Route path="/product/create" element={<CreateProduct />} />
-            <Route path="/product/edit/:id" element={<EditProduct />} />
-            <Route path="/Login/dragLayout" element={<DragLayout />} />
-            <Route exact path='/' element={<ProductList />} />
+            <Route exact path='/' element={<DragLayout />} />
           </Routes>
         </Col>
       </Row>
